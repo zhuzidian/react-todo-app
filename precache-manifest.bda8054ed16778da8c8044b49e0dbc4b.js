2a92c0f3f26dd8741cff5955322b408ee5201f04
Zhu Zidian <zhuzidian@gmail.com>
2020-09-29 18:41:20 +0900

    Updates

---
+++ precache-manifest.bda8054ed16778da8c8044b49e0dbc4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5834842d6a071f24a1005ae767298a1f",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "1143265cf81475c6b19a",
+    "url": "/react-todo-app/static/css/main.15250c41.chunk.css"
+  },
+  {
+    "revision": "25cff88423f8ad155c26",
+    "url": "/react-todo-app/static/js/2.8eaf7c53.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/react-todo-app/static/js/2.8eaf7c53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1143265cf81475c6b19a",
+    "url": "/react-todo-app/static/js/main.4260a1c3.chunk.js"
+  },
+  {
+    "revision": "7f607c3e693deaba8e3a",
+    "url": "/react-todo-app/static/js/runtime-main.93dbfc51.js"
+  }
+]);
